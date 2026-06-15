115256510d8817ba8423185e3999e2a760aac465
lyrad585 <lyrad585@gmail.com>
2026-06-15 19:46:28 -0400

    Apply all-file ignore rule

--- config_logging.py
+++
@@ -1,9 +0,0 @@
-LOGGING_CONFIG = {
-    "file" : "DotaData.log",
-    "locals" : False,
-    "globals" : False,
-    "sleep" : True,
-    "sql_cmds" : False,
-    "sql_params" : False,
-    "traceback" : False
-}
--- import time.py
+++
@@ -1,6 +0,0 @@
-import time
-from tqdm import tqdm
-
-# Simply wrap any iterable with tqdm()
-for i in tqdm(range(100)):
-    time.sleep(0.05)  # Simulating work
